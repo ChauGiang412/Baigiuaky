504e7aafb69578a29e06d10d2d0c19c93cf724da
Chau Giang <nchaugiang9@gmail.com>
2026-09-23 15:02:05 +0700

    baikiemtra

--- Baikiemtra.php
+++ Baikiemtra.php
@@ -54,7 +54,7 @@ $pass = "";
 $dbname = "quanly_hocsinh";
 
 try {
-    $pdo = new PDO("mysql:host=$host", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port", $user,$pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec("CREATE DATABASE IF NOT EXISTS `$dbname` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
     $pdo->exec("USE `$dbname`");
